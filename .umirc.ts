1cec38243c8db822b152910f581177fb665b9058
whyour <imwhyour@gmail.com>
2021-03-29 10:08:19 +0800

    nginx增加gzip压缩，webpack预压缩gz文件

--- .umirc.ts
+++ .umirc.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'umi';
+const CompressionPlugin = require('compression-webpack-plugin');
 
 export default defineConfig({
   hash: true,
@@ -13,5 +14,16 @@ export default defineConfig({
       target: 'http://127.0.0.1:5678/',
       changeOrigin: true,
     },
+  },
+  chainWebpack(memo) {
+    memo.plugin('CompressionPlugin').use(
+      new CompressionPlugin({
+        filename: '[path][base].gz',
+        algorithm: 'gzip',
+        test: /\.js$|\.css$|\.html$/,
+        threshold: 10240,
+        minRatio: 0.8,
+      }),
+    );
   },
 });
