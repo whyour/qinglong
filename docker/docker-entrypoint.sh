57794c8547e582bd8267cb6603340e14e954daeb
anonymous <anonymous@gmail.com>
2021-04-07 20:52:26 +0800

    dev

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo -e "======================1. 更新源代码========================\n"
-update
+update ql
 echo
 
 echo -e "======================2. 检测配置文件========================\n"
@@ -10,36 +10,35 @@ echo -e "======================2. 检测配置文件========================\n"
 
 if [ ! -s ${QL_DIR}/config/crontab.list ]
 then
-  echo -e "检测到config配置目录下不存在crontab.list或存在但文件为空，从示例文件复制一份用于初始化...\n"
-  cp -fv ${QL_DIR}/sample/crontab.list.sample ${QL_DIR}/config/crontab.list
-  sed -i "s,MY_PATH,${QL_DIR},g" ${QL_DIR}/config/crontab.list
-  sed -i "s,ENV_PATH=,PATH=$PATH,g" ${QL_DIR}/config/crontab.list
+    echo -e "检测到config配置目录下不存在crontab.list或存在但文件为空，从示例文件复制一份用于初始化...\n"
+    cp -fv ${QL_DIR}/sample/crontab.sample.list ${QL_DIR}/config/crontab.list
+    perl -i -pe "{s|CMD_UPDATE|update|g; s|CMD_REBUILD|rebuild|g; s|CMD_RMLOG|rmlog|g; s|CMD_TASK|task|g; s|CMD_MYTASK|mytask|g}" ${QL_DIR}/config/crontab.list
 fi
 crontab ${QL_DIR}/config/crontab.list
 echo -e "成功添加定时任务...\n"
 
 if [ ! -s ${QL_DIR}/config/cookie.sh ]; then
-  echo -e "检测到config配置目录下不存在cookie.sh，从示例文件复制一份用于初始化...\n"
-  touch ${QL_DIR}/config/cookie.sh
-  echo
+    echo -e "检测到config配置目录下不存在cookie.sh，从示例文件复制一份用于初始化...\n"
+    touch ${QL_DIR}/config/cookie.sh
+    echo
 fi
 
 if [ ! -s ${QL_DIR}/config/config.sh ]; then
-  echo -e "检测到config配置目录下不存在config.sh，从示例文件复制一份用于初始化...\n"
-  cp -fv ${QL_DIR}/sample/config.sh.sample ${QL_DIR}/config/config.sh
-  echo
+    echo -e "检测到config配置目录下不存在config.sh，从示例文件复制一份用于初始化...\n"
+    cp -fv ${QL_DIR}/sample/config.sample.sh ${QL_DIR}/config/config.sh
+    echo
 fi
 
 if [ ! -s ${QL_DIR}/config/auth.sample.json ]; then
-  echo -e "检测到config配置目录下不存在auth.json，从示例文件复制一份用于初始化...\n"
-  cp -fv ${QL_DIR}/sample/auth.sample.json ${QL_DIR}/config/auth.json
-  echo
+    echo -e "检测到config配置目录下不存在auth.json，从示例文件复制一份用于初始化...\n"
+    cp -fv ${QL_DIR}/sample/auth.sample.json ${QL_DIR}/config/auth.json
+    echo
 fi
 
 if [ -s /etc/nginx/conf.d/default.conf ]; then
-  echo -e "检测到默认nginx配置文件，删除...\n"
-  rm -f /etc/nginx/conf.d/default.conf
-  echo
+    echo -e "检测到默认nginx配置文件，删除...\n"
+    rm -f /etc/nginx/conf.d/default.conf
+    echo
 fi
 
 cp -fv ${QL_DIR}/docker/front.conf /etc/nginx/conf.d/front.conf
