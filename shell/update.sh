0cf9882bb3516cecc4b3008df52ebd58e423fa78
whyour <imwhyour@gmail.com>
2021-05-12 20:09:19 +0800

    修复ql update状态异常

--- shell/update.sh
+++ shell/update.sh
@@ -255,10 +255,11 @@ update_qinglong() {
     fi
 
     if [[ $exit_status -eq 0 ]]; then
-        echo -e "重启青龙面板...\n"
+        echo -e "重启青龙面板(后台重启)...\n"
         cd $dir_root
         nginx -s reload 2>/dev/null || nginx -c /etc/nginx/nginx.conf
-        echo -e "重启面板完成...\n"
+        echo -e "重启面板完成(请5s后刷新页面)..."
+        pm2 reload all
     fi
 }
 
