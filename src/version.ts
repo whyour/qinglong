203862c870157820f15647f4d06180a56c439324
hanhh <hanhh@gmail.com>
2021-06-08 22:10:12 +0800

    更新版本号

--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
-export const version = 'v2.2.0';
-export const changeLog = 'https://t.me/jiaolongwang/102';
+export const version = 'v2.2.0-061';
+export const changeLog = 'https://t.me/jiaolongwang/104';
