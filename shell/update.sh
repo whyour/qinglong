031f1bc1ca6a1abbf51496e4dd994fc0591fcdb5
hanhh <18330117883@163.com>
2021-06-13 20:04:56 +0800

    暂时回退ql repo拉取

--- shell/update.sh
+++ shell/update.sh
@@ -356,16 +356,16 @@ gen_list_repo() {
 
     cd ${repo_path}
     
-    local cmd="find ."
-    local index=0
-    for extension in $file_extensions; do
-        if [[ $index -eq 0 ]]; then
-            cmd="${cmd} -name \"*.${extension}\""
-        else
-            cmd="${cmd} -o -name \"*.${extension}\""
-        fi
-        let index+=1
-    done
+    local cmd="find . -name *.js"
+    # local index=0
+    # for extension in $file_extensions; do
+    #     if [[ $index -eq 0 ]]; then
+    #         cmd="${cmd} -name \"*.${extension}\""
+    #     else
+    #         cmd="${cmd} -o -name \"*.${extension}\""
+    #     fi
+    #     let index+=1
+    # done
     files=$($cmd | sed 's/^..//')
     if [[ $path ]]; then
         files=$(echo "$files" | egrep $path)
