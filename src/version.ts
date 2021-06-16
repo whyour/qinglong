b15a13fea3485e89f61f439988bbf48b95906f4b
hanhh <hanhh@gmail.com>
2021-06-16 23:58:19 +0800

    更新版本号

--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
-export const version = 'v2.2.0-064';
-export const changeLog = 'https://t.me/jiaolongwang/107';
+export const version = 'v2.2.0-065';
+export const changeLog = 'https://t.me/jiaolongwang/108';
