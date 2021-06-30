32b357c7fd84430ffb32fbbcc4cbfe6d6c0e8057
hanhh <hanhh@gmail.com>
2021-06-30 12:16:05 +0800

    修复token唯一性验证

--- back/config/util.ts
+++ back/config/util.ts
@@ -27,7 +27,7 @@ export function getLastModifyFilePath(dir: string) {
   return filePath;
 }
 
-export function createPassword(min: number, max: number): string {
+export function createRandomString(min: number, max: number): string {
   const num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   const english = [
     'a',
