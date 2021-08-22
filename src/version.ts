f49f605c1157898c63d019af6a4cf3b5e2b52747
hanhh <18330117883@163.com>
2021-08-22 15:23:58 +0800

    更新版本号

--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
-export const version = 'v2.8.4-001';
-export const changeLog = 'https://t.me/jiaolongwang/133';
+export const version = 'v2.8.4-002';
+export const changeLog = 'https://t.me/jiaolongwang/134';
