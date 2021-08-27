cad714493e55018e61e9990f284f2c04db029077
hanhh <18330117883@163.com>
2021-08-27 11:08:08 +0800

    更新版本号

--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
-export const version = 'v2.8.5-002';
-export const changeLog = 'https://t.me/jiaolongwang/139';
+export const version = 'v2.9.0';
+export const changeLog = 'https://t.me/jiaolongwang/141';
