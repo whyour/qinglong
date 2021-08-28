3a998a37f0b9b8a3a31e9ceb07446ecd9dee3b1d
hanhh <18330117883@163.com>
2021-08-29 00:52:21 +0800

    增加nedb数据自定比对，测试对db文件不断增大的影响

--- back/services/cron.ts
+++ back/services/cron.ts
@@ -23,6 +23,7 @@ export default class CronService {
     this.cronDb.loadDatabase((err) => {
       if (err) throw err;
     });
+    this.cronDb.persistence.setAutocompactionInterval(30000);
   }
 
   public getDb(): DataStore {
