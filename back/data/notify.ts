f2dac51a623ceac16b2333432ca80a0dcbcbdc55
hanhh <18330117883@163.com>
2021-09-17 00:35:27 +0800

    更新通知服务

--- back/data/notify.ts
+++ back/data/notify.ts
@@ -16,65 +16,66 @@ abstract class NotificationBaseInfo {
 }
 
 export class GoCqHttpBotNotification extends NotificationBaseInfo {
-  public GOBOT_URL = '';
-  public GOBOT_TOKEN = '';
-  public GOBOT_QQ = '';
+  public goCqHttpBotUrl = '';
+  public goCqHttpBotToken = '';
+  public goCqHttpBotQq = '';
 }
 
 export class ServerChanNotification extends NotificationBaseInfo {
-  public SCKEY = '';
+  public serverChanKey = '';
 }
 
 export class BarkNotification extends NotificationBaseInfo {
-  public BARK_PUSH = '';
-  public BARK_SOUND = '';
-  public BARK_GROUP = 'qinglong';
+  public barkPush = '';
+  public barkSound = '';
+  public barkGroup = 'qinglong';
 }
 
 export class TelegramBotNotification extends NotificationBaseInfo {
-  public TG_BOT_TOKEN = '';
-  public TG_USER_ID = '';
-  public TG_PROXY_HOST = '';
-  public TG_PROXY_PORT = '';
-  public TG_PROXY_AUTH = '';
-  public TG_API_HOST = 'api.telegram.org';
+  public telegramBotToken = '';
+  public telegramBotUserId = '';
+  public telegramBotProxyHost = '';
+  public telegramBotProxyPort = '';
+  public telegramBotProxyAuth = '';
+  public telegramBotApiHost = 'api.telegram.org';
 }
 
 export class DingtalkBotNotification extends NotificationBaseInfo {
-  public DD_BOT_TOKEN = '';
-  public DD_BOT_SECRET = '';
+  public dingtalkBotToken = '';
+  public dingtalkBotSecret = '';
 }
 
 export class WeWorkBotNotification extends NotificationBaseInfo {
-  public QYWX_KEY = '';
+  public weWorkBotKey = '';
 }
 
 export class WeWorkAppNotification extends NotificationBaseInfo {
-  public QYWX_AM = '';
+  public weWorkAppKey = '';
 }
 
 export class IGotNotification extends NotificationBaseInfo {
-  public IGOT_PUSH_KEY = '';
+  public iGotPushKey = '';
 }
 
 export class PushPlusNotification extends NotificationBaseInfo {
-  public PUSH_PLUS_TOKEN = '';
-  public PUSH_PLUS_USER = '';
+  public pushPlusToken = '';
+  public pushPlusUser = '';
 }
 
 export class EmailNotification extends NotificationBaseInfo {
-  public service: string = '';
-  public user: string = '';
-  public pass: string = '';
+  public emailService: string = '';
+  public emailUser: string = '';
+  public emailPass: string = '';
 }
 
-export type NotificationInfo =
-  | GoCqHttpBotNotification
-  | ServerChanNotification
-  | BarkNotification
-  | TelegramBotNotification
-  | DingtalkBotNotification
-  | WeWorkBotNotification
-  | IGotNotification
-  | PushPlusNotification
-  | EmailNotification;
+export interface NotificationInfo
+  extends GoCqHttpBotNotification,
+    ServerChanNotification,
+    BarkNotification,
+    TelegramBotNotification,
+    DingtalkBotNotification,
+    WeWorkBotNotification,
+    WeWorkAppNotification,
+    IGotNotification,
+    PushPlusNotification,
+    EmailNotification {}
--- back/services/notify.ts
+++ back/services/notify.ts
@@ -2,9 +2,16 @@ import { NotificationInfo } from '../data/notify';
 import { Service, Inject } from 'typedi';
 import winston from 'winston';
 import AuthService from './auth';
+import got from 'got';
+import nodemailer from 'nodemailer';
+import crypto from 'crypto';
+import { HttpProxyAgent, HttpsProxyAgent } from 'hpagent';
 
 @Service()
 export default class NotificationService {
+  @Inject((type) => AuthService)
+  private authService!: AuthService;
+
   private modeMap = new Map([
     ['goCqHttpBot', this.goCqHttpBot],
     ['serverChan', this.serverChan],
@@ -18,14 +25,12 @@ export default class NotificationService {
     ['email', this.email],
   ]);
 
+  private timeout = 10000;
   private title = '';
   private content = '';
   private params!: Omit<NotificationInfo, 'type'>;
 
-  constructor(
-    @Inject('logger') private logger: winston.Logger,
-    private authService: AuthService,
-  ) {}
+  constructor(@Inject('logger') private logger: winston.Logger) {}
 
   public async notify(title: string, content: string) {
     const { type, ...rest } = await this.authService.getNotificationMode();
@@ -38,22 +43,257 @@ export default class NotificationService {
     }
   }
 
-  private async goCqHttpBot() {}
+  private async goCqHttpBot() {
+    const { goCqHttpBotQq, goCqHttpBotToken, goCqHttpBotUrl } = this.params;
+    const res: any = await got
+      .post(
+        `${goCqHttpBotUrl}?access_token=${goCqHttpBotToken}&${goCqHttpBotQq}`,
+        {
+          timeout: this.timeout,
+          retry: 0,
+          json: { message: `${this.title}\n${this.content}` },
+        },
+      )
+      .json();
+    return res.retcode === 0;
+  }
 
-  private async serverChan() {}
+  private async serverChan() {
+    const { serverChanKey } = this.params;
+    const url = serverChanKey.includes('SCT')
+      ? `https://sctapi.ftqq.com/${SCKEY}.send`
+      : `https://sc.ftqq.com/${SCKEY}.send`;
+    const res: any = await got
+      .post(url, {
+        timeout: this.timeout,
+        retry: 0,
+        body: `text=${this.title}&desp=${this.content}`,
+      })
+      .json();
+    return res.errno === 0 || res.data.errno === 0;
+  }
 
-  private async bark() {}
+  private async bark() {
+    const { barkPush, barkSound, barkGroup } = this.params;
+    const url = `${barkPush}/${encodeURIComponent(
+      this.title,
+    )}/${encodeURIComponent(
+      this.content,
+    )}?sound=${barkSound}&group=${barkGroup}`;
+    const res: any = await got
+      .get(url, {
+        timeout: this.timeout,
+        retry: 0,
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      })
+      .json();
+    return res.code === 200;
+  }
 
-  private async telegramBot() {}
+  private async telegramBot() {
+    const {
+      telegramBotApiHost,
+      telegramBotProxyAuth,
+      telegramBotProxyHost,
+      telegramBotProxyPort,
+      telegramBotToken,
+      telegramBotUserId,
+    } = this.params;
+    const url = `https://${telegramBotApiHost}/bot${telegramBotToken}/sendMessage`;
+    const options = {
+      keepAlive: true,
+      keepAliveMsecs: 1000,
+      maxSockets: 256,
+      maxFreeSockets: 256,
+      proxy: `http://${telegramBotProxyHost}:${telegramBotProxyPort}`,
+    };
+    const httpAgent = new HttpProxyAgent(options);
+    const httpsAgent = new HttpsProxyAgent(options);
+    const res: any = await got
+      .post(url, {
+        timeout: this.timeout,
+        retry: 0,
+        body: `chat_id=${telegramBotUserId}&text=${this.title}\n\n${this.content}&disable_web_page_preview=true`,
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        agent: {
+          http: httpAgent,
+          https: httpsAgent,
+        },
+      })
+      .json();
+    return !!res.ok;
+  }
 
-  private async dingtalkBot() {}
+  private async dingtalkBot() {
+    const { dingtalkBotSecret, dingtalkBotToken } = this.params;
+    let secretParam = '';
+    if (dingtalkBotSecret) {
+      const dateNow = Date.now();
+      const hmac = crypto.createHmac('sha256', dingtalkBotSecret);
+      hmac.update(`${dateNow}\n${dingtalkBotSecret}`);
+      const result = encodeURIComponent(hmac.digest('base64'));
+      secretParam = `&timestamp=${dateNow}&sign=${result}`;
+    }
+    const url = `https://oapi.dingtalk.com/robot/send?access_token=${dingtalkBotToken}${secretParam}`;
+    const res: any = await got
+      .post(url, {
+        timeout: this.timeout,
+        retry: 0,
+        json: {
+          msgtype: 'text',
+          text: {
+            content: ` ${this.title}\n\n${this.content}`,
+          },
+        },
+      })
+      .json();
+    return res.errcode === 0;
+  }
 
-  private async weWorkBot() {}
+  private async weWorkBot() {
+    const { weWorkBotKey } = this.params;
+    const url = `https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${weWorkBotKey}`;
+    const res: any = await got
+      .post(url, {
+        timeout: this.timeout,
+        retry: 0,
+        json: {
+          msgtype: 'text',
+          text: {
+            content: ` ${this.title}\n\n${this.content}`,
+          },
+        },
+      })
+      .json();
+    return res.errcode === 0;
+  }
 
-  private async weWorkApp() {}
-  private async iGot() {}
+  private async weWorkApp() {
+    const { weWorkAppKey } = this.params;
+    const [corpid, corpsecret, touser, agentid, thumb_media_id = '1'] =
+      weWorkAppKey.split(',');
+    const url = `https://qyapi.weixin.qq.com/cgi-bin/gettoken`;
+    const { access_token } = await got
+      .post(url, {
+        timeout: this.timeout,
+        retry: 0,
+        json: {
+          corpid,
+          corpsecret,
+        },
+      })
+      .json();
 
-  private async pushPlus() {}
+    let options: any = {
+      msgtype: 'mpnews',
+      mpnews: {
+        articles: [
+          {
+            title: `${this.title}`,
+            thumb_media_id,
+            author: `智能助手`,
+            content_source_url: ``,
+            content: `${this.content.replace(/\n/g, '<br/>')}`,
+            digest: `${this.content}`,
+          },
+        ],
+      },
+    };
+    switch (thumb_media_id) {
+      case '0':
+        options = {
+          msgtype: 'textcard',
+          textcard: {
+            title: `${this.title}`,
+            description: `${this.content}`,
+            url: 'https://github.com/whyour/qinglong',
+            btntxt: '更多',
+          },
+        };
+        break;
 
-  private async email() {}
+      case '1':
+        options = {
+          msgtype: 'text',
+          text: {
+            content: `${this.title}\n\n${this.content}`,
+          },
+        };
+        break;
+    }
+
+    const res: any = await got
+      .post(
+        `https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=${access_token}`,
+        {
+          timeout: this.timeout,
+          retry: 0,
+          json: {
+            touser,
+            agentid,
+            safe: '0',
+            ...options,
+          },
+        },
+      )
+      .json();
+
+    return res.errcode === 0;
+  }
+
+  private async iGot() {
+    const { iGotPushKey } = this.params;
+    const url = `https://push.hellyw.com/${iGotPushKey.toLowerCase()}`;
+    const res: any = await got
+      .post(url, {
+        timeout: this.timeout,
+        retry: 0,
+        body: `title=${this.title}&content=${this.content}`,
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      })
+      .json();
+
+    return res.ret === 0;
+  }
+
+  private async pushPlus() {
+    const { pushPlusToken, pushPlusUser } = this.params;
+    const url = `https://www.pushplus.plus/send`;
+    const res: any = await got
+      .post(url, {
+        timeout: this.timeout,
+        retry: 0,
+        json: {
+          token: `${pushPlusToken}`,
+          title: `${this.title}`,
+          content: `${this.content.replace(/[\n\r]/g, '<br>')}`,
+          topic: `${pushPlusUser}`,
+        },
+      })
+      .json();
+
+    return res.code === 200;
+  }
+
+  private async email() {
+    const { emailPass, emailService, emailUser } = this.params;
+    const transporter = nodemailer.createTransport({
+      service: emailService,
+      auth: {
+        user: emailUser,
+        pass: emailPass,
+      },
+    });
+
+    const info = await transporter.sendMail({
+      from: `"青龙快讯" <${emailUser}>`,
+      to: `${emailUser}`,
+      subject: `${this.title}`,
+      html: `${this.content.replace(/\n/g, '<br/>')}`,
+    });
+
+    transporter.close();
+
+    return !!info.messageId;
+  }
 }
