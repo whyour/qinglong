e5313e63a964140acc61ae4b39ac0a2b67ed398d
hanhh <18330117883@163.com>
2021-09-19 23:00:53 +0800

    更新版本号

--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
-export const version = 'v2.9.2-002';
-export const changeLog = 'https://t.me/jiao_long/177';
+export const version = 'v2.9.3';
+export const changeLog = 'https://t.me/jiao_long/178';
