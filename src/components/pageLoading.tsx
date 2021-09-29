ecaf731ecf04d4f527884804c09f7522584c9006
hanhh <18330117883@163.com>
2021-09-29 21:19:25 +0800

    修复loading状态位置

--- src/components/pageLoading.tsx
+++ src/components/pageLoading.tsx
@@ -1,3 +1,11 @@
 import { PageLoading } from '@ant-design/pro-layout';
 
-export default PageLoading;
+const NewPageLoading = () => {
+  return (
+    <div style={{ margin: '-24px -24px 0' }}>
+      <PageLoading delay={1}></PageLoading>
+    </div>
+  );
+};
+
+export default NewPageLoading;
