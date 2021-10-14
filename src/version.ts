43ddc54a708555e3fe24132fdd503e5b3d729082
hanhh <18330117883@163.com>
2021-10-14 22:07:21 +0800

    更新版本 2.9.8

--- src/version.ts
+++ src/version.ts
@@ -1,7 +1,11 @@
-export const version = '2.9.7';
-export const changeLogLink = 'https://t.me/jiao_long/184';
-export const changeLog = `2.9.7版本说明
-1. 重构初始化流程，可直接在初始化过程中设置账户信息和通知方式
-2. 增加企业微信机器人推送的python版本，感谢 https://github.com/Sowevo  PR
-3. 修复ql repo可能无法拉取最新代码
+export const version = '2.9.8';
+export const changeLogLink = 'https://t.me/jiao_long/185';
+export const changeLog = `2.9.8版本说明
+1. 增加系统更新功能(系统设置-其他设置-检查更新)
+2. 增加系统设置日志自动删除功能(系统设置-其他设置-日志删除频率)
+3. task conc命令支持指定账号(task xxx.js conc 1-2 3 4)
+4. 修复初始化通知设置，优化UI
+5. 表格滚动固定表头和尾部分页
+6. 环境变量名称增加校验，不能输入中划线
+7. 容器启动忽略config.sh和env.sh中错误，避免造成容器一直重启
 `;
