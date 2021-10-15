838326616bfe300695684d6a1520cafda66a0fd9
hanhh <18330117883@163.com>
2021-10-15 11:47:32 +0800

    更新版本号 v2.9.8-001

--- src/version.ts
+++ src/version.ts
@@ -1,11 +1,6 @@
-export const version = '2.9.8';
-export const changeLogLink = 'https://t.me/jiao_long/185';
-export const changeLog = `2.9.8版本说明
-1. 增加系统更新功能(系统设置-其他设置-检查更新)
-2. 增加系统设置日志自动删除功能(系统设置-其他设置-日志删除频率)
-3. task conc命令支持指定账号(task xxx.js conc 1-2 3 4)
-4. 修复初始化通知设置，优化UI
-5. 表格滚动固定表头和尾部分页
-6. 环境变量名称增加校验，不能输入中划线
-7. 容器启动忽略config.sh和env.sh中错误，避免造成容器一直重启
+export const version = '2.9.8-001';
+export const changeLogLink = 'https://t.me/jiao_long/187';
+export const changeLog = `2.9.8-001版本说明
+1. 修复nginx配置文件
+2. 修复设置删除日志频率
 `;
