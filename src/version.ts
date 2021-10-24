160dbe780cb3e9e53469b9a3605add8f56c98df7
whyour <imwhyour@gmail.com>
2021-10-24 17:51:20 +0800

    更新版本号 v2.10.0

--- src/version.ts
+++ src/version.ts
@@ -1,8 +1,8 @@
-export const version = '2.9.10';
-export const changeLogLink = 'https://t.me/jiao_long/190';
-export const changeLog = `2.9.10版本说明
-1. 修复系统通知telegram代理认证参数
-2. 修复ql update错误
+export const version = '2.10.0';
+export const changeLogLink = 'https://t.me/jiao_long/191';
+export const changeLog = `2.10.0 版本说明
+1. 添加依赖管理功能
+2. 修复python3任务无法停止
 3. 修复table滚动高度
-4. 更新安装说明
+4. 其他优化
 `;
