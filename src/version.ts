e959466898d3eaef7082e05f2e6bd0cc05d29d22
whyour <imwhyour@gmail.com>
2021-11-04 15:09:07 +0800

    更新版本号 v2.10.1

--- src/version.ts
+++ src/version.ts
@@ -1,8 +1,10 @@
-export const version = '2.10.0';
-export const changeLogLink = 'https://t.me/jiao_long/191';
-export const changeLog = `2.10.0 版本说明
-1. 添加依赖管理功能
-2. 修复python3任务无法停止
-3. 修复table滚动高度
-4. 其他优化
+export const version = '2.10.1';
+export const changeLogLink = 'https://t.me/jiao_long/192';
+export const changeLog = `2.10.1 版本说明
+1. 增加白屏时间及错误上报
+2. 退出登录移到左下角，增加用户名展示
+3. 修复编辑定时任务后置顶失效
+4. 修复依赖管理无法覆盖安装
+5. 修复表格滚动高度
+6. 修复ql check命令
 `;
