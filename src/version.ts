b7b983229b08877d7a7e884c82544e46c26612db
whyour <imwhyour@gmail.com>
2021-11-04 18:10:43 +0800

    更新版本号 v2.10.2

--- src/version.ts
+++ src/version.ts
@@ -1,10 +1,6 @@
-export const version = '2.10.1';
-export const changeLogLink = 'https://t.me/jiao_long/192';
-export const changeLog = `2.10.1 版本说明
-1. 增加白屏时间及错误上报
-2. 退出登录移到左下角，增加用户名展示
-3. 修复编辑定时任务后置顶失效
-4. 修复依赖管理无法覆盖安装
-5. 修复表格滚动高度
-6. 修复ql check命令
+export const version = '2.10.2';
+export const changeLogLink = 'https://t.me/jiao_long/193';
+export const changeLog = `2.10.2 版本说明
+1. 修复openapi跳转
+2. 修复基础镜像依赖 pip3
 `;
