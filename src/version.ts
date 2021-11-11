9a43e022e9bf965c1f89500e88465926caf0e7a0
whyour <imwhyour@gmail.com>
2021-11-11 23:04:29 +0800

    修改版本说明序号

--- src/version.ts
+++ src/version.ts
@@ -3,10 +3,10 @@ export const changeLogLink = 'https://t.me/jiao_long/194';
 export const changeLog = `2.10.3 版本说明
 1. ql repo拉取脚本存储在scripts下的子目录中，task命令运行时指定相对目录
 2. 脚本管理改为树结构展示
-2. 裁剪数据库文件大于500兆，防止服务无法启动
-3. 更新python通知文件，感谢 https://github.com/Oreomeow PR
-4. 修复移动端导航
+3. 裁剪数据库文件大于500兆，防止服务无法启动
+4. 更新python通知文件，感谢 https://github.com/Oreomeow PR
+5. 修复移动端导航
 5. 修复任务执行时间0点显示24点
-6. 修复centos兼容性
-7. 其他bug修复
+7. 修复centos兼容性
+8. 其他bug修复
 `;
