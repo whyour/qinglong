18cd7bfa6ff3ad23e647a122c0b0b958d4c60bfc
whyour <imwhyour@gmail.com>
2021-11-12 13:18:32 +0800

    更新版本号

--- src/version.ts
+++ src/version.ts
@@ -1,12 +1,5 @@
-export const version = '2.10.3';
-export const changeLogLink = 'https://t.me/jiao_long/194';
-export const changeLog = `2.10.3 版本说明
-1. ql repo拉取脚本存储在scripts下的子目录中，task命令运行时指定相对目录
-2. 脚本管理改为树结构展示
-3. 裁剪数据库文件大于500兆，防止服务无法启动
-4. 更新python通知文件，感谢 https://github.com/Oreomeow PR
-5. 修复移动端导航
-5. 修复任务执行时间0点显示24点
-7. 修复centos兼容性
-8. 其他bug修复
+export const version = '2.10.4';
+export const changeLogLink = 'https://t.me/jiao_long/195';
+export const changeLog = `2.10.4 版本说明
+1. 修复容器启动失败（可能包含arm玩客云）
 `;
