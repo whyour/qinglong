fce6a996d9ef1b9a290714bddb3ecb0a9b6f8282
whyour <imwhyour@gmail.com>
2021-11-12 14:23:16 +0800

    更新版本号  v2.10.5

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,5 @@
-export const version = '2.10.4';
-export const changeLogLink = 'https://t.me/jiao_long/195';
-export const changeLog = `2.10.4 版本说明
-1. 修复容器启动失败（可能包含arm玩客云）
+export const version = '2.10.5';
+export const changeLogLink = 'https://t.me/jiao_long/196';
+export const changeLog = `2.10.5 版本说明
+1. 修复截取db文件判断，引起服务异常
 `;
