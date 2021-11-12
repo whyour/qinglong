4e95d62933b301b2d1c93620985a2c624d4acf28
whyour <imwhyour@gmail.com>
2021-11-13 00:03:12 +0800

    更新版本 v2.10.6

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,9 @@
-export const version = '2.10.5';
-export const changeLogLink = 'https://t.me/jiao_long/196';
-export const changeLog = `2.10.5 版本说明
-1. 修复截取db文件判断，引起服务异常
+export const version = '2.10.6';
+export const changeLogLink = 'https://t.me/jiao_long/225';
+export const changeLog = `2.10.6 版本说明
+1. 增加各版本自动打包workflow，感谢 https://github.com/fzls PR
+2. 环境变量添加更新时间，感谢 https://github.com/miniers PR
+3. 修复脚本管理结构
+4. 修复定时匹配规则
+5. 修复检查更新
 `;
