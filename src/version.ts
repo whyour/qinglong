39979879c9ae398d60dafcbe6e6eb31eef78bbc8
whyour <imwhyour@gmail.com>
2021-11-13 21:21:35 +0800

    更新版本 v2.10.7

--- src/version.ts
+++ src/version.ts
@@ -1,9 +1,10 @@
-export const version = '2.10.6';
-export const changeLogLink = 'https://t.me/jiao_long/225';
-export const changeLog = `2.10.6 版本说明
-1. 增加各版本自动打包workflow，感谢 https://github.com/fzls PR
-2. 环境变量添加更新时间，感谢 https://github.com/miniers PR
-3. 修复脚本管理结构
-4. 修复定时匹配规则
-5. 修复检查更新
+export const version = '2.10.7';
+export const changeLogLink = 'https://t.me/jiao_long/226';
+export const changeLog = `2.10.7 版本说明
+1. repo命令默认给仓库添加sendNotify依赖
+2. 增加 /ql/deps 目录，此目录下的依赖文件会覆盖系统默认和仓库内的依赖文件
+3. 修复脚本管理列表及搜索
+4. 修复环境变量手机端列表样式
+5. 修复秒级定时任务服务
+6. 修复bot启动命令
 `;
