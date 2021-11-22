fa018b84a70722f11315a7802ef549301103d17c
whyour <imwhyour@gmail.com>
2021-11-22 22:27:15 +0800

    更新版本 v2.10.9

--- src/version.ts
+++ src/version.ts
@@ -1,9 +1,10 @@
-export const version = '2.10.8';
-export const changeLogLink = 'https://t.me/jiao_long/227';
-export const changeLog = `2.10.8 版本说明
-1. 脚本管理新建文件增加选择父目录
-2. 修复脚本管理更新文件
-3. 增加gotify推送，感谢 https://github.com/kilo5hz PR
-4. 修复不能复制deps目录文件
-5. 修复可能的玩客云问题
+export const version = '2.10.9';
+export const changeLogLink = 'https://t.me/jiao_long/228';
+export const changeLog = `2.10.9 版本说明
+1. 任务管理支持任务名跳转脚本管理页，感谢 https://github.com/kilo5hz PR
+2. 系统通知支持gotify，感谢 https://github.com/kilo5hz PR
+3. 定时任务列表pageSize增加200/500/1000，感谢 https://github.com/fzls PR
+4. 修复deps目录依赖文件拷贝
+5. 修改alpine基础镜像版本，解决arm32位系统无法启动容器，感谢 https://github.com/lx200916
+6. 修复调试功能
 `;
