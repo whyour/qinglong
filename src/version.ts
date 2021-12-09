3f12d9cbd59f51115fea7d8f77f145cc02518f8e
whyour <imwhyour@gmail.com>
2021-12-09 23:24:10 +0800

    更新版本号 v2.10.12

--- src/version.ts
+++ src/version.ts
@@ -1,8 +1,6 @@
-export const version = '2.10.11';
-export const changeLogLink = 'https://t.me/jiao_long/230';
-export const changeLog = `2.10.11 版本说明
-1. 修复环境变量中包含双引号变量异常，感谢 https://github.com/mengshouer
-2. 修复最后运行时长覆盖逻辑
-3. 修复内容区高度和logo样式
-4. 修复搜索urlencode的文本时，结果异常
+export const version = '2.10.12';
+export const changeLogLink = 'https://t.me/jiao_long/232';
+export const changeLog = `2.10.12 版本说明
+1. 修复logo样式
+2. 修复暗黑模式无法访问
 `;
