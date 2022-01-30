16532720d7f590bffb8e463b640f7aeb67868ae1
whyour <imwhyour@gmail.com>
2022-01-30 18:25:18 +0800

    更新版本 v2.11.2

--- src/version.ts
+++ src/version.ts
@@ -1,13 +1,12 @@
-export const version = '2.11.1';
+export const version = '2.11.2';
 export const changeLogLink = 'https://t.me/jiao_long/261';
-export const changeLog = `2.11.1 版本说明
-1. 修复openapi获取token，token认证
-2. 修复环境变量搜索，排序，更新
-3. 修复重置应用秘钥
-4. 修复环境变量更新时间显示
-5. 修复定时默认任务排序
-6. task支持运行pyc文件，感谢https://github.com/chiupam
-6. 移除ghproxy默认代理，增加新配置ProxyUrl，支持http或者socks代理。例如 http://127.0.0.1:7890
-7. 更换JavaScript cdn地址
-8. 其他bug修复
+export const changeLog = `2.11.2 版本说明
+1. 修复更新通知
+2. 修复定时任务排序
+3. 移除cdn文件
+4. 修复日志加载状态
+5. python任务测试实时输出
+6. 修改定时任务、环境变量唯一值
+6. 修复shell克隆仓库状态获取
+7. 重构基础镜像，缩减镜像大小、打包时间
 `;
