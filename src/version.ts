4d1354d5f4a153b9fbf591121964f3a29b375550
whyour <imwhyour@gmail.com>
2022-02-16 23:09:33 +0800

    更新版本 v2.11.3

--- src/version.ts
+++ src/version.ts
@@ -1,12 +1,11 @@
-export const version = '2.11.2';
-export const changeLogLink = 'https://t.me/jiao_long/261';
-export const changeLog = `2.11.2 版本说明
-1. 修复更新通知
-2. 修复定时任务排序
-3. 移除cdn文件
-4. 修复日志加载状态
-5. python任务测试实时输出
-6. 修改定时任务、环境变量唯一值
-6. 修复shell克隆仓库状态获取
-7. 重构基础镜像，缩减镜像大小、打包时间
+export const version = '2.11.3';
+export const changeLogLink = 'https://t.me/jiao_long/262';
+export const changeLog = `2.11.3 版本说明
+1. 修复定时任务排序
+2. 环境变量增加更新时间排序
+3. 修复logo地址
+4. 修改check命令
+5. 安装依赖默认使用Proxy_url代理地址
+6. 修改apk源地址
+7. bark支持自定义图标，感谢 https://github.com/Ukenn2112 PR
 `;
