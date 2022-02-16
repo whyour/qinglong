86df495b67d32c80de4fc173ba94e7a04ebc2343
whyour <imwhyour@gmail.com>
2022-02-16 23:06:12 +0800

    修改check命令，安装依赖使用代理
    修改apk源地址

--- shell/check.sh
+++ shell/check.sh
@@ -10,13 +10,11 @@ reset_env() {
   echo -e "---> 配置文件检测完成\n"
 
   echo -e "---> 2. 开始安装青龙依赖\n"
-  rm -rf $dir_root/node_modules
   npm_install_2 $dir_root
   echo -e "---> 青龙依赖安装完成\n"
 
   echo -e "---> 3. 开始安装脚本依赖\n"
   cp -f $dir_sample/package.json $dir_scripts/package.json
-  rm -rf $dir_scripts/node_modules
   npm_install_2 $dir_scripts
   echo -e "---> 脚本依赖安装完成\n"
 }
@@ -50,8 +48,8 @@ pm2_log() {
   echo -e "---> pm2日志"
   local panelOut="/root/.pm2/logs/panel-out.log"
   local panelError="/root/.pm2/logs/panel-error.log"
-  tail -n 20 "$panelOut"
-  tail -n 20 "$panelError"
+  tail -n 100 "$panelOut"
+  tail -n 100 "$panelError"
 }
 
 check_nginx() {
