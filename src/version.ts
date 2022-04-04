5f6c0882d64b7fcdf8e622d9db42ba120e848bcf
whyour <imwhyour@gmail.com>
2022-04-04 17:28:15 +0800

    修改版本说明

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 export const version = '2.12.1';
 export const changeLogLink = 'https://t.me/jiao_long/289';
-export const changeLog = `2.12.0 版本说明
+export const changeLog = `2.12.1 版本说明
 1. 修复定时任务详情获取日志
 2. 修复系统通知pushdeer
 3. 修复python pushDeer推送，感谢 https://github.com/chen310 PR
