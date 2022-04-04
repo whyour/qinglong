58e047fe0e5c419c9d09a20e25618f12e4e6cacd
whyour <imwhyour@gmail.com>
2022-04-04 23:32:27 +0800

    修复task日志打印

--- shell/task.sh
+++ shell/task.sh
@@ -366,7 +366,7 @@ main() {
             esac
             ;;
         esac
-        [[ -f $log_path ]] && cat $log_path
+        [[ -f $log_path ]] && cat "$dir_log/$log_path"
     elif [[ $# -eq 0 ]]; then
         echo
         usage
