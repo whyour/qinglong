6907d520eda54d94da7bedff418484c41e0f0291
whyour <imwhyour@gmail.com>
2022-04-05 23:11:48 +0800

    增加发布脚本

---
+++ shell/pub.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+echo -e "开始发布"
+
+echo -e "切换master分支"
+git checkout master
+
+echo -e "合并develop代码"
+git merge origin/develop
+
+echo -e "提交master代码"
+git push
+
+echo -e "更新cdn文件"
+ts-node sample/tool.ts
+
+string = $(cat src/version.ts | grep "version" | egrep "[^\']*" -o | egrep "\d\.*")
+version = "v$string"
+echo -e "当前版本$version"
+
+echo -e "删除已经存在的本地tag"
+git tag -d "$version" &>/dev/null
+
+echo -e "删除已经存在的远程tag"
+
+echo -e "创建新tag"
+git tag -a "$version" -m "release $version"
+
+echo -e "提交tag"
+git push --tags
+
+echo -e "完成发布"
