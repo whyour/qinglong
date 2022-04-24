84357e9dccebf30c7e388cbf69ff8a6bd6190224
whyour <imwhyour@gmail.com>
2022-04-24 17:50:03 +0800

    更新版本 v2.12.2

--- src/version.ts
+++ src/version.ts
@@ -1,10 +1,10 @@
-export const version = '2.12.1';
-export const changeLogLink = 'https://t.me/jiao_long/289';
-export const changeLog = `2.12.1 版本说明
-1. 修复定时任务详情获取日志
-2. 修复系统通知pushdeer
-3. 修复python pushDeer推送，感谢 https://github.com/chen310 PR
-3. nodejs telegram推送改为json方式，感谢 https://github.com/kan3Git PR
-4. 修复日志目录拼接规则
-5. 其他bug修复
+export const version = '2.12.2';
+export const changeLogLink = 'https://t.me/jiao_long/290';
+export const changeLog = `2.12.2 版本说明
+1. 任务详情支持运行、禁用、置顶操作
+2. 依赖管理增加直接强制删除
+3. 修复环境变量引号转义逻辑，感谢 https://github.com/JerryWn12 PR
+4. 修复定时删除日志设置，支持设置为24天以上
+5. 修复拉取脚本，shell发送通知
+6. 修复shell获取日志目录
 `;
