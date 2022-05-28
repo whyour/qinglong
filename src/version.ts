99d7eaeff03f3025db036e31c669e724ba0dfe3b
whyour <imwhyour@gmail.com>
2022-05-29 01:50:46 +0800

    更新版本 v2.13.1

--- src/version.ts
+++ src/version.ts
@@ -1,12 +1,8 @@
-export const version = '2.13.0';
+export const version = '2.13.1';
 export const changeLogLink = 'https://t.me/jiao_long/303';
-export const changeLog = `2.13.0 版本说明
-1. 新增订阅管理模块，支持公开仓库/私有仓库等
-2. 支持修改头像
-3. 修复特殊字符环境变量异常
-4. 修改定时任务添加标签
-5. 修复添加任务默认会展示一个空标签
-6. 修复用户名显示
-7. 修复依赖安装
-8. 其他bug修复
+export const changeLog = `2.13.1 版本说明
+1. 修复新建订阅repo命令快捷导入
+2. 修复执行订阅任务含有before/after命令时，造成面板无法访问
+3. 修复任务详情日志列表访问
+4. 修复安装依赖造成CPU满负荷，服务器崩溃
 `;
