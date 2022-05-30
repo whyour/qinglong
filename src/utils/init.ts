8c74fca362f4d6b2099b619b6cb2e85b5040fc00
whyour <imwhyour@gmail.com>
2022-05-30 09:58:51 +0800

    修复新建订阅定时

--- src/utils/init.ts
+++ src/utils/init.ts
@@ -7,9 +7,15 @@ export function init() {
   // sentry监控 init
   Sentry.init({
     dsn: 'https://3406424fb1dc4813a62d39e844a9d0ac@o1098464.ingest.sentry.io/6122818',
-    integrations: [new Integrations.BrowserTracing()],
+    integrations: [
+      new Integrations.BrowserTracing({
+        shouldCreateSpanForRequest(url) {
+          return !url.includes('/api/ws') && !url.includes('/api/static');
+        },
+      }),
+    ],
     release: version,
-    tracesSampleRate: 1.0,
+    tracesSampleRate: 0.1,
     beforeBreadcrumb(breadcrumb, hint?) {
       if (breadcrumb.data && breadcrumb.data.url) {
         const url = breadcrumb.data.url.replace(/token=.*/, '');
