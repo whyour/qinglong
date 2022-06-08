f152136f0b933a3641e5b4aecb75203c2311c66d
whyour <imwhyour@gmail.com>
2022-06-08 18:39:51 +0800

    更新版本 v2.13.3

--- src/version.ts
+++ src/version.ts
@@ -1,8 +1,9 @@
-export const version = '2.13.2';
+export const version = '2.13.3';
 export const changeLogLink = 'https://t.me/jiao_long/303';
-export const changeLog = `2.13.2 版本说明
-1. 修复nodejs依赖安装失败pnpm setup
-2. 修复订阅禁用失效
-3. 修复新建订阅定时验证
-4. 修复ssh配置文件更新逻辑
+export const changeLog = `2.13.3 版本说明
+1. 新建脚本支持文件上传
+2. 修复调试脚本运行，不需要每次保存
+3. 修复新建脚本选择嵌套目录
+4. 修复粘贴订阅，私有仓库字段不更新
+5. 修复脚本管理、日志管理树
 `;
