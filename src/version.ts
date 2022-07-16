9f0503f1387e44318b4ccf7322f3cc6a15706d7d
whyour <imwhyour@gmail.com>
2022-07-16 12:31:50 +0800

    更新版本 v2.13.4

--- src/version.ts
+++ src/version.ts
@@ -1,9 +1,14 @@
-export const version = '2.13.3';
+export const version = '2.13.4';
 export const changeLogLink = 'https://t.me/jiao_long/303';
-export const changeLog = `2.13.3 版本说明
-1. 新建脚本支持文件上传
-2. 修复调试脚本运行，不需要每次保存
-3. 修复新建脚本选择嵌套目录
-4. 修复粘贴订阅，私有仓库字段不更新
-5. 修复脚本管理、日志管理树
+export const changeLog = `2.13.4 版本说明
+1. 增加系统token，防止未登录时无法更新仓库和任务状态
+2. 脚本管理按照类型排序
+3. 任务详情中编辑脚本支持全屏
+4. 环境变量增加导出功能
+5. 修复无法更新脚本内容为空
+6. 修复停止node脚本报错
+7. 修改手动停止任务耗时
+8. 修复脚本管理、日志管理树
+9. 修复获取环境变量详情
+10. 区分原生termux和termux中安装的Linux发，感谢 erma0
 `;
