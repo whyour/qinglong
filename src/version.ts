c0977f9e372779ff93d84b332a3acf7126dfb544
whyour <imwhyour@gmail.com>
2022-07-17 22:29:25 +0800

    更新版本 v2.13.5

--- src/version.ts
+++ src/version.ts
@@ -1,14 +1,5 @@
-export const version = '2.13.4';
-export const changeLogLink = 'https://t.me/jiao_long/303';
-export const changeLog = `2.13.4 版本说明
-1. 增加系统token，防止未登录时无法更新仓库和任务状态
-2. 脚本管理按照类型排序
-3. 任务详情中编辑脚本支持全屏
-4. 环境变量增加导出功能
-5. 修复无法更新脚本内容为空
-6. 修复停止node脚本报错
-7. 修改手动停止任务耗时
-8. 修复脚本管理、日志管理树
-9. 修复获取环境变量详情
-10. 区分原生termux和termux中安装的Linux发，感谢 erma0
+export const version = '2.13.5';
+export const changeLogLink = 'https://t.me/jiao_long/320';
+export const changeLog = `2.13.5 版本说明
+1. 修复task命令获取时间异常
 `;
