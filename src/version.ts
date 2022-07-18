29f5134fb48e5e5848232fed497c0e12c20d2890
whyour <imwhyour@gmail.com>
2022-07-18 19:52:37 +0800

    更新版本 v2.13.6

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,5 @@
-export const version = '2.13.5';
-export const changeLogLink = 'https://t.me/jiao_long/320';
-export const changeLog = `2.13.5 版本说明
-1. 修复task命令获取时间异常
+export const version = '2.13.6';
+export const changeLogLink = 'https://t.me/jiao_long/321';
+export const changeLog = `2.13.6 版本说明
+1. 修复 cant't find .env
 `;
