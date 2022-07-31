53414e5d7077281d2d46ec8ace291573bb05c5e8
whyour <imwhyour@gmail.com>
2022-07-31 15:58:34 +0800

    更新版本 v2.13.7

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,8 @@
-export const version = '2.13.6';
-export const changeLogLink = 'https://t.me/jiao_long/321';
-export const changeLog = `2.13.6 版本说明
-1. 修复 cant't find .env
+export const version = '2.13.7';
+export const changeLogLink = 'https://t.me/jiao_long/322';
+export const changeLog = `2.13.7 版本说明
+1. 修复typescript脚本引用sendNotify文件，感谢 charmingYouYou
+2. 修复订阅管理搜索
+3. 修复环境变量\n转义
+4. repo命令后缀参数改为竖线分割
 `;
