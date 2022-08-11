06723407d2560883e09bf32537610a286dbf7e5a
whyour <imwhyour@gmail.com>
2022-08-11 19:41:59 +0800

    更新版本 v2.13.8

--- src/version.ts
+++ src/version.ts
@@ -1,8 +1,5 @@
-export const version = '2.13.7';
-export const changeLogLink = 'https://t.me/jiao_long/322';
-export const changeLog = `2.13.7 版本说明
-1. 修复typescript脚本引用sendNotify文件，感谢 charmingYouYou
-2. 修复订阅管理搜索
-3. 修复环境变量\n转义
-4. repo命令后缀参数改为竖线分割
+export const version = '2.13.8';
+export const changeLogLink = 'https://t.me/jiao_long/323';
+export const changeLog = `2.13.8 版本说明
+1. 修改系统token访问逻辑，加快任务启动速度
 `;
