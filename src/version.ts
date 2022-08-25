32eec68278bca44d61bed367ff8605a1ac572877
whyour <imwhyour@gmail.com>
2022-08-26 00:04:59 +0800

    更新版本 v2.13.9

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,7 @@
-export const version = '2.13.8';
-export const changeLogLink = 'https://t.me/jiao_long/323';
-export const changeLog = `2.13.8 版本说明
-1. 修改系统token访问逻辑，加快任务启动速度
+export const version = '2.13.9';
+export const changeLogLink = 'https://t.me/jiao_long/324';
+export const changeLog = `2.13.9 版本说明
+1. 修改定时任务分页功能，加快每页数据获取
+2. 定时任务增加每页数据可设置为最大，使任务一页展示（数据获取速度也会变慢）
+3. favicon修改😀😀
 `;
