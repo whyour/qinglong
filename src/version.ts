70571a9d34d33f37b91f58faf5c8a7a76aec1dfd
whyour <imwhyour@gmail.com>
2022-09-04 18:28:28 +0800

    更新版本 v2.14.1

--- src/version.ts
+++ src/version.ts
@@ -1,8 +1,7 @@
-export const version = '2.14.0';
-export const changeLogLink = 'https://t.me/jiao_long/325';
-export const changeLog = `2.14.0 版本说明
-1. 定时任务自定义视图功能来啦，支持自定义筛选条件、排序规则等
-2. 配置文件可设置需要延迟的脚本后缀，默认只有js脚本随机延迟执行，感谢 https://github.com/cddjr
-3. 修复定时任务搜索及搜索分页
-4. 其他bug修复
+export const version = '2.14.1';
+export const changeLogLink = 'https://t.me/jiao_long/326';
+export const changeLog = `2.14.1 版本说明
+1. 修复定时任务搜索
+2. 修复视图切换默认页码
+3. 增加群辉chat通知方式，感谢 https://github.com/Appoip
 `;
