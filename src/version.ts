54d99a49549a8b414c54241e8254c3805b7d7b1c
whyour <imwhyour@gmail.com>
2022-09-07 19:30:19 +0800

    更新版本 v2.14.2

--- src/version.ts
+++ src/version.ts
@@ -1,7 +1,9 @@
-export const version = '2.14.1';
-export const changeLogLink = 'https://t.me/jiao_long/326';
-export const changeLog = `2.14.1 版本说明
-1. 修复定时任务搜索
-2. 修复视图切换默认页码
-3. 增加群辉chat通知方式，感谢 https://github.com/Appoip
+export const version = '2.14.2';
+export const changeLogLink = 'https://t.me/jiao_long/327';
+export const changeLog = `2.14.2 版本说明
+1. 增加npm镜像源配置 NpmMirror="https://registry.npmmirror.com"
+2. 修复任务视图筛选条件和编辑视图，感谢 https://github.com/cddjr
+3. 修复获取多级脚本路径错误，感谢 https://github.com/cddjr
+4. 修改定时任务详情交互，点击任务名称打开任务详情，点击任务命令跳转脚本
+5. 其他bug修复
 `;
