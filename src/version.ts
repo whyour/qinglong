04037f29e54f0e93a0ae097c8107d403d55c8b2b
whyour <imwhyour@gmail.com>
2022-09-10 14:12:26 +0800

    更新版本 v2.14.3

--- src/version.ts
+++ src/version.ts
@@ -1,9 +1,6 @@
-export const version = '2.14.2';
-export const changeLogLink = 'https://t.me/jiao_long/327';
-export const changeLog = `2.14.2 版本说明
-1. 增加npm镜像源配置 NpmMirror="https://registry.npmmirror.com"
-2. 修复任务视图筛选条件和编辑视图，感谢 https://github.com/cddjr
-3. 修复获取多级脚本路径错误，感谢 https://github.com/cddjr
-4. 修改定时任务详情交互，点击任务名称打开任务详情，点击任务命令跳转脚本
-5. 其他bug修复
+export const version = '2.14.3';
+export const changeLogLink = 'https://t.me/jiao_long/330';
+export const changeLog = `2.14.8 版本说明
+1. 修复安装依赖可能出现的  ERR_PNPM_REGISTRIES_MISMATCH
+2. 今人不见古时月，今月曾经照古人  -> 中秋快乐
 `;
