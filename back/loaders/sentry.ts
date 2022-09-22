e274d3e2f99d6d8dd59625fc0d249744f2fb652e
whyour <imwhyour@gmail.com>
2022-09-22 12:14:42 +0800

    sentry增加版本号

--- back/loaders/sentry.ts
+++ back/loaders/sentry.ts
@@ -2,16 +2,23 @@ import { Application } from 'express';
 import * as Sentry from '@sentry/node';
 import * as Tracing from '@sentry/tracing';
 import Logger from './logger';
+import config from '../config';
+import fs from 'fs';
 
 export default ({ expressApp }: { expressApp: Application }) => {
+  const versionRegx = /.*export const version = \'(.*)\'\;/;
+
+  const currentVersionFile = fs.readFileSync(config.versionFile, 'utf8');
+  const currentVersion = currentVersionFile.match(versionRegx)![1];
+  
   Sentry.init({
     dsn: 'https://f4b5b55fb3c645b29a5dc2d70a1a4ef4@o1098464.ingest.sentry.io/6122819',
     integrations: [
       new Sentry.Integrations.Http({ tracing: true }),
       new Tracing.Integrations.Express({ app: expressApp }),
     ],
-
-    tracesSampleRate: 1.0,
+    tracesSampleRate: 0.1,
+    release: currentVersion,
   });
 
   expressApp.use(Sentry.Handlers.requestHandler());
