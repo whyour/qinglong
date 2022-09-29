88cf671465a0d1244132eec8eaaeeb8dfba2383e
whyour <imwhyour@gmail.com>
2022-09-29 23:06:31 +0800

    更新版本v2.14.6

--- src/version.ts
+++ src/version.ts
@@ -1,9 +1,8 @@
-export const version = '2.14.5';
+export const version = '2.14.6';
 export const changeLogLink = 'https://t.me/jiao_long/333';
-export const changeLog = `2.14.5 版本说明
-1. 日志管理支持删除日志目录和日志文件
-2. 脚本管理支持删除文件夹
-3. 修复任务详情获取不到日志列表
-4. 修复移动端脚本高亮
-5. 修复安装Node依赖提示ERR_PNPM_REGISTRIES_MISMATCH
+export const changeLog = `2.14.6 版本说明
+1. 任务执行日志增加资源占用说明
+2. 配置文件config.sh增加资源配置参数CpuWarn/MemoryWarn/DiskWarn
+3. 修复脚本管理和日志管理搜索
+4. 其他bug修复
 `;
