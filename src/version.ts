9c6d9ec22dbf67b526f4f82b06dde7c4f6ff7931
whyour <imwhyour@gmail.com>
2022-10-22 18:36:38 +0800

    更新版本 v2.14.8

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,8 @@
-export const version = '2.14.7';
-export const changeLogLink = 'https://t.me/jiao_long/334';
-export const changeLog = `2.14.7 版本说明
-1. 修复task_before无法获取任务执行参数
-2. 修改资源判定逻辑
+export const version = '2.14.8';
+export const changeLogLink = 'https://t.me/jiao_long/335';
+export const changeLog = `2.14.8 版本说明
+1. 支持环境变量导入，导入格式参考导出文件
+2. 修复创建任务，命令中的引号丢失
+3. 修复调试选择脚本及语言识别
+4. 修复ql和task命令日志打印
 `;
