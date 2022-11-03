543c241bc6af4d0ff4571cd05ce6405a7244fe65
whyour <imwhyour@gmail.com>
2022-11-03 21:16:46 +0800

    更新版本 v2.14.10

--- src/version.ts
+++ src/version.ts
@@ -1,9 +1,10 @@
-export const version = '2.14.9';
-export const changeLogLink = 'https://t.me/jiao_long/336';
-export const changeLog = `2.14.9 版本说明
-1. 通知支持飞书和智能微秘书，感谢 https://github.com/leochen-g
-2. webhook通知body支持嵌套json
-3. 修复shell获取磁盘占用
-4. 修复定时任务移动端滚动
-5. 其他bug修复
+export const version = '2.14.10';
+export const changeLogLink = 'https://t.me/jiao_long/337';
+export const changeLog = `2.14.10 版本说明
+1. 环境变量值支持快捷复制
+2. 修复环境变量位置移动算法
+3. 修复 notify.js 飞书通知
+4. 修复 task 参数转义
+5. 修复资源预警通知
+6. 其他bug修复
 `;
