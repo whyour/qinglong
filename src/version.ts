fae7fad915c055cb5b7549a567d370921885e899
whyour <imwhyour@gmail.com>
2022-11-15 12:15:06 +0800

    更新版本 v2.15.0

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,11 @@
-export const version = '2.14.12';
-export const changeLogLink = 'https://t.me/jiao_long/339';
-export const changeLog = `2.14.12 版本说明
-1. 修复可能出现移动或者创建环境变量出错
+export const version = '2.15.0';
+export const changeLogLink = 'https://t.me/jiao_long/340';
+export const changeLog = `2.15.0 版本说明
+1. 任务视图筛选条件支持 且/或
+2. 订阅支持设置代理
+3. 修改日志管理列表默认排序
+4. 修复openapi token生成逻辑
+5. 修复日志管理搜索失效
+6. 修复系统最后commit时间获取
+7. 其他优化
 `;
