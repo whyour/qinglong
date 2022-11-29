8a7d6a1c1397aad18e63bba720d5654aedf6a43a
whyour <imwhyour@gmail.com>
2022-11-29 21:01:28 +0800

    修改资源检查逻辑

--- shell/otask.sh
+++ shell/otask.sh
@@ -75,7 +75,8 @@ run_nohup() {
 }
 
 check_server() {
-  cpu_use=$(top -b -n 1 | grep CPU | grep -v -E 'grep|PID' | awk '{print $2}' | cut -f 1 -d "%" | head -n 1)
+  local top_result = $(top -b -n 1)
+  cpu_use=$(echo $top_result | grep CPU | grep -v -E 'grep|PID' | awk '{print $2}' | cut -f 1 -d "%" | head -n 1)
 
   mem_free=$(free -m | grep "Mem" | awk '{print $3}' | head -n 1)
   mem_total=$(free -m | grep "Mem" | awk '{print $2}' | head -n 1)
@@ -84,7 +85,7 @@ check_server() {
   disk_use=$(df -P | grep /dev | grep -v -E '(tmp|boot|shm)' | awk '{print $5}' | cut -f 1 -d "%" | head -n 1)
 
   if [[ $cpu_use -gt $cpu_warn ]] || [[ $mem_free -lt $mem_warn ]] || [[ $disk_use -gt $disk_warn ]]; then
-    local resource=$(top -b -n 1 | grep -v -E 'grep|Mem|idle|Load|tr' | awk '{$2="";$3="";$4="";$5="";$7="";print $0}' | head -n 10 | tr '\n' '|' | sed s/\|/\\\\n/g)
+    local resource=$(echo $top_result | grep -v -E 'grep|Mem|idle|Load|tr' | awk '{$2="";$3="";$4="";$5="";$7="";print $0}' | head -n 10 | tr '\n' '|' | sed s/\|/\\\\n/g)
     notify_api "服务器资源异常警告" "当前CPU占用 $cpu_use% 内存占用 $mem_use% 磁盘占用 $disk_use% \n资源占用详情 \n\n $resource"
   fi
 }
