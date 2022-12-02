468d05cb10caab366712dbed8736da7b953467b0
whyour <imwhyour@gmail.com>
2022-12-02 18:07:54 +0800

    更新版本 v2.15.2

--- src/version.ts
+++ src/version.ts
@@ -1,8 +1,8 @@
-export const version = '2.15.1';
-export const changeLogLink = 'https://t.me/jiao_long/341';
-export const changeLog = `2.15.1 版本说明
-1. 系统更新增加 gitee 更新源
-2. pushDeer 通知增加自架版
-3. 修复移动端最后更新时间展示
+export const version = '2.15.2';
+export const changeLogLink = 'https://t.me/jiao_long/353';
+export const changeLog = `2.15.2 版本说明
+1. 任务视图增加系统视图排序
+2. task 命令增加 -m 参数，支持设置任务超时时间，格式参考 CommandTimeoutTime。例如 task -m 1h xxx.js
+3. 修复资源占用检查逻辑
 4. 其他优化
 `;
