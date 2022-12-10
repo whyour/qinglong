3afb1d18f622fbd36bd14fd5fd0678a704c77dab
whyour <imwhyour@gmail.com>
2022-12-10 19:06:39 +0800

    更新版本 v2.15.3

--- src/version.ts
+++ src/version.ts
@@ -1,8 +1,10 @@
-export const version = '2.15.2';
-export const changeLogLink = 'https://t.me/jiao_long/353';
-export const changeLog = `2.15.2 版本说明
-1. 任务视图增加系统视图排序
-2. task 命令增加 -m 参数，支持设置任务超时时间，格式参考 CommandTimeoutTime。例如 task -m 1h xxx.js
-3. 修复资源占用检查逻辑
-4. 其他优化
+export const version = '2.15.3';
+export const changeLogLink = 'https://t.me/jiao_long/354';
+export const changeLog = `2.15.3 版本说明
+1. 任务视图增加标签筛选
+2. 修改默认镜像python版本为3.10
+3. 修复notify.js中智能微秘书，感谢 https://github.com/CoolClash
+4. 修复超时任务日志打印
+5. 修复调试脚本页保存脚本父目录
+6. 其他优化
 `;
