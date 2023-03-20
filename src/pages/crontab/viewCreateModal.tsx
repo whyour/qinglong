8a225e9bebe9025c3ba89683d9fc29ed6ebbfe1f
whyour <imwhyour@gmail.com>
2023-03-20 18:03:09 +0800

    修复视图筛选条件样式

--- src/pages/crontab/viewCreateModal.tsx
+++ src/pages/crontab/viewCreateModal.tsx
@@ -232,9 +232,6 @@ const ViewCreateModal = ({
                     <Space
                       className="view-create-modal-filters"
                       align="baseline"
-                      style={
-                        fields.length > 1 ? { width: 'calc(100% - 40px)' } : {}
-                      }
                     >
                       <Form.Item
                         {...restField}
