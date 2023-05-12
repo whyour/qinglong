407618fa04dbf4f337d8651571ba2258d003174f
whyour <imwhyour@gmail.com>
2023-05-12 14:18:22 +0800

    增加 debian-slim 基础镜像

--- back/services/cron.ts
+++ back/services/cron.ts
@@ -80,7 +80,7 @@ export default class CronService {
       return doc;
     }
 
-    if (this.isNodeCron(doc) && !this.isSpecialSchedule(doc.schedule)) {
+    if (!this.isSpecialSchedule(doc.schedule)) {
       await cronClient.addCron([
         {
           name: doc.name || '',
@@ -111,11 +111,9 @@ export default class CronService {
       return newDoc;
     }
 
-    if (this.isNodeCron(doc)) {
-      await cronClient.delCron([String(doc.id)]);
-    }
+    await cronClient.delCron([String(newDoc.id)]);
 
-    if (this.isNodeCron(newDoc) && !this.isSpecialSchedule(newDoc.schedule)) {
+    if (!this.isSpecialSchedule(newDoc.schedule)) {
       await cronClient.addCron([
         {
           name: doc.name || '',
@@ -577,20 +575,19 @@ export default class CronService {
   public async enabled(ids: number[]) {
     await CrontabModel.update({ isDisabled: 0 }, { where: { id: ids } });
     const docs = await CrontabModel.findAll({ where: { id: ids } });
-    const sixCron = docs
-      .filter((x) => this.isNodeCron(x) && !this.isSpecialSchedule(x.schedule))
-      .map((doc) => ({
-        name: doc.name || '',
-        id: String(doc.id),
-        schedule: doc.schedule!,
-        command: this.makeCommand(doc),
-        extra_schedules: doc.extra_schedules || [],
-      }));
+    const crons = docs.map((doc) => ({
+      name: doc.name || '',
+      id: String(doc.id),
+      schedule: doc.schedule!,
+      command: this.makeCommand(doc),
+      extra_schedules: doc.extra_schedules || [],
+    }));
 
     if (isDemoEnv()) {
       return;
     }
-    await cronClient.addCron(sixCron);
+    
+    await cronClient.addCron(crons);
     await this.setCrontab();
   }
 
@@ -690,13 +687,6 @@ export default class CronService {
 
     await writeFileWithLock(config.crontabFile, crontab_string);
 
-    try {
-      execSync(`crontab ${config.crontabFile}`);
-    } catch (error: any) {
-      const errorMsg = error.message || String(error);
-      this.logger.error('[crontab] Failed to update system crontab:', errorMsg);
-    }
-
     await CrontabModel.update({ saved: true }, { where: {} });
   }
 
@@ -742,12 +732,7 @@ export default class CronService {
   public async autosave_crontab() {
     const tabs = await this.crontabs();
     const regularCrons = tabs.data
-      .filter(
-        (x) =>
-          x.isDisabled !== 1 &&
-          this.isNodeCron(x) &&
-          !this.isSpecialSchedule(x.schedule),
-      )
+      .filter((x) => x.isDisabled !== 1 && !this.isSpecialSchedule(x.schedule))
       .map((doc) => ({
         name: doc.name || '',
         id: String(doc.id),
--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -50,6 +50,6 @@ fi
 
 log_with_style "SUCCESS" "🎉  容器启动成功!"
 
-crond -f >/dev/null
+tail -f /dev/null
 
 exec "$@"
