0bbb7c7b8947085fac1d9ed758216017c79c5a07
whyour <imwhyour@gmail.com>
2023-05-30 17:45:47 +0800

    修复获取 cpu 核数

--- back/shared/pLimit.ts
+++ back/shared/pLimit.ts
@@ -1,7 +1,7 @@
 import pLimit from "p-limit";
 import os from 'os';
 
-const cronLimit = pLimit(os.cpus.length || 3);
+const cronLimit = pLimit(os.cpus().length);
 
 export function runCronWithLimit<T>(fn: () => Promise<T>): Promise<T> {
   return cronLimit(() => {
