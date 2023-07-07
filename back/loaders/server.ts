3aa112e373167d50c50a7c3f17d33d08de85ef50
whyour <imwhyour@gmail.com>
2023-07-08 00:27:05 +0800

    修改 node 进程错误监控

--- back/loaders/server.ts
+++ back/loaders/server.ts
@@ -5,18 +5,34 @@ import Sock from './sock';
 export default async ({ server }: { server: Server }) => {
   await Sock({ server });
   Logger.info('✌️ Sock loaded');
+  let exitTime = 0;
+  let timer: NodeJS.Timeout;
 
-  process.on('SIGINT', () => {
-    Logger.info('✌️ Server need close');
-    server.close(() => {
-      setTimeout(() => {
-        process.exit();
-      }, 10000);
-    });
-
-    setTimeout(() => {
-      console.log('Forcing server close !!!');
+  process.on('SIGINT', (singal) => {
+    Logger.warn(`Server need close, singal ${singal}`);
+    exitTime++;
+    if (exitTime >= 3) {
+      Logger.warn('Forcing server close');
+      clearTimeout(timer);
       process.exit(1);
-    }, 15000);
+    }
+    server.close(() => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        process.exit();
+      }, 15000);
+    });
+  });
+
+  process.on('uncaughtException', (error) => {
+    Logger.error('Uncaught exception:', error);
+    process.exit(1);
+  });
+  
+  process.on('unhandledRejection', (reason, promise) => {
+    Logger.error('Unhandled rejection:', reason);
+    process.exit(1);
   });
 };
