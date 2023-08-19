f3514f5a693136907bc3cf9b14868cd39201065a
dream10201 <xiuxiu10201@gmail.com>
2023-08-19 18:23:08 +0800

    修复linux依赖检测 (#2082)

--- back/services/dependence.ts
+++ back/services/dependence.ts
@@ -277,7 +277,7 @@ export default class DependenceService {
             depInfo &&
             ((isNodeDependence && depInfo.split(' ')?.[0] === depName) ||
               (isLinuxDependence &&
-                depInfo.toLocaleLowerCase().includes('installed')) ||
+                depInfo.toLocaleLowerCase().includes('apt-manual-installed')) ||
               isPythonDependence) &&
             (!depVersion || depInfo.includes(depVersion))
           ) {
