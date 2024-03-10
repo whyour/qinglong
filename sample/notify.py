2ca1f7cf010b0f3f106d45bdd0b1f1ee1be15a23
flyingfeng <2917268843@qq.com>
2024-03-10 18:37:27 +0800

    【补充】更新飞书 bot 验证过程，兼容新状态码

--- sample/notify.py
+++ sample/notify.py
@@ -221,7 +221,7 @@ def feishu_bot(title: str, content: str, **kwargs) -> None:
     data = {"msg_type": "text", "content": {"text": f"{title}\n\n{content}"}}
     response = requests.post(url, data=json.dumps(data)).json()
 
-    if response.get("StatusCode") == 0:
+    if response.get("StatusCode") == 0 or response.get("code") == 0:
         print("飞书 推送成功！")
     else:
         print("飞书 推送失败！错误信息如下：\n", response)
