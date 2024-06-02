101b8258f884c68f75ed226c1809b6d385c8ede2
whyour <imwhyour@gmail.com>
2024-06-02 16:22:47 +0800

    修改 npm 安装启动命令

--- shell/start.sh
+++ shell/start.sh
@@ -16,14 +16,14 @@ if [[ ! $QL_DIR ]]; then
   fi
 
   if [[ $QL_DIR ]]; then
-    echo -e "请先设置 export QL_DIR=$QL_DIR，环境变量，并添加到系统环境变量，然后再次执行命令 qinglong 启动服务"
+    echo -e "请先手动设置 export QL_DIR=$QL_DIR，环境变量，并手动添加到系统环境变量，然后再次执行命令 qinglong 启动服务"
   fi
 
   exit 1
 fi
 
 if [[ ! $QL_DATA_DIR ]]; then
-  echo -e "请先设置数据存储目录 export QL_DATA_DIR 环境变量，目录必须以斜杠开头的绝对路径，并添加到系统环境变量"
+  echo -e "请先手动设置数据存储目录 export QL_DATA_DIR 环境变量，目录必须以斜杠开头的绝对路径，并手动添加到系统环境变量"
   exit 1
 fi
 
@@ -56,8 +56,7 @@ else
   exit 1
 fi
 
-npm install -g pnpm@8.3.1
-pnpm add -g pm2 tsx
+npm install -g pnpm@8.3.1 pm2 tsx
 
 cd ${QL_DIR}
 cp -f .env.example .env
@@ -66,6 +65,7 @@ chmod 777 ${QL_DIR}/shell/*.sh
 . ${QL_DIR}/shell/env.sh
 . ${QL_DIR}/shell/share.sh
 
+echo -e "======================1. 检测配置文件========================\n"
 make_dir /etc/nginx/conf.d
 make_dir /run/nginx
 init_nginx
@@ -73,16 +73,28 @@ fix_config
 
 pm2 l &>/dev/null
 
+echo -e "======================2. 安装依赖========================\n"
 patch_version
 
+echo -e "======================3. 启动nginx========================\n"
+nginx -s reload 2>/dev/null || nginx -c /etc/nginx/nginx.conf
+echo -e "nginx启动成功...\n"
+
+reload_update
 reload_pm2
 
-nginx -s reload 2>/dev/null || nginx -c /etc/nginx/nginx.conf
-
 if [[ $AutoStartBot == true ]]; then
-  nohup ql -l bot >$dir_log/bot.log 2>&1 &
+  echo -e "======================5. 启动bot========================\n"
+  nohup ql bot >$dir_log/bot.log 2>&1 &
+  echo -e "bot后台启动中...\n"
 fi
 
 if [[ $EnableExtraShell == true ]]; then
-  nohup ql -l extra >$dir_log/extra.log 2>&1 &
+  echo -e "====================6. 执行自定义脚本========================\n"
+  nohup ql extra >$dir_log/extra.log 2>&1 &
+  echo -e "自定义脚本后台执行中...\n"
 fi
+
+echo -e "############################################################\n"
+echo -e "启动完成..."
+echo -e "############################################################\n"
