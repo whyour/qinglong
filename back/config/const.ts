3188abd0ea05b44d196766b4214d5fa4c67b2e70
whyour <imwhyour@gmail.com>
2024-06-30 00:47:41 +0800

    安装 linux 依赖自动识别 alpine 和 debian

--- back/config/const.ts
+++ back/config/const.ts
@@ -23,3 +23,38 @@ export const SAMPLE_FILES = [
     target: 'data/scripts/notify.py',
   },
 ];
+
+export const LINUX_DEPENDENCE_COMMAND: Record<
+  'Debian' | 'Ubuntu' | 'Alpine',
+  {
+    install: string;
+    uninstall: string;
+    info: string;
+    check(info: string): boolean;
+  }
+> = {
+  Debian: {
+    install: 'apt install -y',
+    uninstall: 'apt remove -y',
+    info: 'apt info',
+    check(info: string) {
+      return info.includes('apt-manual-installed');
+    },
+  },
+  Ubuntu: {
+    install: 'apt install -y',
+    uninstall: 'apt remove -y',
+    info: 'apt info',
+    check(info: string) {
+      return info.includes('apt-manual-installed');
+    },
+  },
+  Alpine: {
+    install: 'apk add --no-check-certificate',
+    uninstall: 'apk del',
+    info: 'apk info -es',
+    check(info: string) {
+      return info.includes('installed');
+    },
+  },
+};
