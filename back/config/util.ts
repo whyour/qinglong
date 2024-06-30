50e8e2c9cc63a58f4bda6f54ef479daf9806c3ec
whyour <imwhyour@gmail.com>
2024-06-30 13:39:31 +0800

    修改 apt 命令

--- back/config/util.ts
+++ back/config/util.ts
@@ -691,7 +691,7 @@ async function _updateLinuxMirror(
           currentDomainWithScheme,
           mirrorDomainWithScheme || 'http://deb.debian.org',
         );
-        return 'apt update';
+        return 'apt-get update';
       } else {
         throw Error(`Current mirror domain not found.`);
       }
@@ -704,7 +704,7 @@ async function _updateLinuxMirror(
           currentDomainWithScheme,
           mirrorDomainWithScheme || 'http://archive.ubuntu.com',
         );
-        return 'apt update';
+        return 'apt-get update';
       } else {
         throw Error(`Current mirror domain not found.`);
       }
