4fa5fa201430f14148f8f20f17c31e9355a7119a
whyour <imwhyour@gmail.com>
2025-01-16 00:35:47 +0800

    修改 lock 文件名称规则

--- back/shared/utils.ts
+++ back/shared/utils.ts
@@ -1,27 +1,39 @@
 import { lock } from 'proper-lockfile';
+import os from 'os';
+import path from 'path';
 import { writeFile, open } from 'fs/promises';
 import { fileExist } from '../config/util';
 
+function getUniqueLockPath(filePath: string) {
+  const sanitizedPath = filePath
+    .replace(/[<>:"/\\|?*]/g, '_')
+    .replace(/^_/, '');
+  return path.join(os.tmpdir(), `${sanitizedPath}.ql_lock`);
+}
+
 export async function writeFileWithLock(
-  path: string,
+  filePath: string,
   content: string | Buffer,
   options: Parameters<typeof writeFile>[2] = {},
 ) {
   if (typeof options === 'string') {
     options = { encoding: options };
   }
-  if (!(await fileExist(path))) {
-    const fileHandle = await open(path, 'w');
+  if (!(await fileExist(filePath))) {
+    const fileHandle = await open(filePath, 'w');
     fileHandle.close();
   }
-  const release = await lock(path, {
+  const lockfilePath = getUniqueLockPath(filePath);
+
+  const release = await lock(filePath, {
     retries: {
       retries: 10,
       factor: 2,
       minTimeout: 100,
       maxTimeout: 3000,
     },
+    lockfilePath,
   });
-  await writeFile(path, content, { encoding: 'utf8', ...options });
+  await writeFile(filePath, content, { encoding: 'utf8', ...options });
   await release();
 }
