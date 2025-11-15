d01ec3b31035ccfc6664149cb756e5bf01a87674
whyour <imwhyour@gmail.com>
2025-11-15 19:09:07 +0800

    修复 parse cron

--- src/pages/crontab/modal.tsx
+++ src/pages/crontab/modal.tsx
@@ -3,7 +3,7 @@ import config from '@/utils/config';
 import { request } from '@/utils/http';
 import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 import { Button, Form, Input, Modal, Select, Space, message } from 'antd';
-import cronParse from 'cron-parser';
+import { CronExpressionParser } from 'cron-parser';
 import { useEffect, useState } from 'react';
 import intl from 'react-intl-universal';
 import { getScheduleType, scheduleTypeMap } from './const';
@@ -91,7 +91,7 @@ const CronModal = ({
             { required: true },
             {
               validator: (_, value) => {
-                if (!value || cronParse.parseExpression(value).hasNext()) {
+                if (!value || CronExpressionParser.parse(value).hasNext()) {
                   return Promise.resolve();
                 }
                 return Promise.reject(intl.get('Cron表达式格式有误'));
